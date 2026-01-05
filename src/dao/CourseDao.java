73d11704d752f45f3b540d7cca77424feaea3a07
Jonathan Maier <jonathan.maier@fms-ea.com>
2026-01-05 14:57:44 +0100

    ajout d'une methode pour trouver tous les cours (findall)

--- src/dao/CourseDao.java
+++ src/dao/CourseDao.java
@@ -1,11 +1,22 @@
 package dao;
 
+import config.DatabaseConfig;
 import entity.Course;
 
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+/**
+* Dao for Course entity
+**/
 public class CourseDao implements IDao<Course>{
+    private static final Logger LOGGER = Logger.getLogger(CourseDao.class.getName());
+    private static CourseDao instance;
+    private DatabaseConfig dbConfig;
+
     @Override
     public Course save(Course entity) throws SQLException {
         return null;
@@ -26,8 +37,32 @@ public class CourseDao implements IDao<Course>{
         return null;
     }
 
+    private Course mapResultSetToCourse(ResultSet rs) throws SQLException {
+        Course course = new Course();
+        course.setId(rs.getLong("id_Course"));
+        course.setName(rs.getString("co_name"));
+        course.setDescription(rs.getString("co_description"));
+        course.setDuration(rs.getInt("co_duration"));
+        course.setType(rs.getString("co_type"));
+        course.setPrice(rs.getDouble("co_price"));
+        return course;
+    }
+
     @Override
     public List<Course> findAll() {
-        return List.of();
+        String sql = "SELECT * FROM courses";
+        List<Course> courses = new ArrayList<>();
+
+        try (Connection connect = dbConfig.getConnection();
+             Statement statement = connect.createStatement();
+             ResultSet rs = statement.executeQuery(sql)) {
+
+            while (rs.next()) {
+                courses.add(mapResultSetToCourse(rs));
+            }
+        } catch (SQLException e){
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+        }
+        return courses;
     }
 }

f55e802234efd7a5e7d98bda9caa610ad6b4d02f
Jonathan Maier <jonathan.maier@fms-ea.com>
2026-01-05 11:32:36 +0100

    ajout des getter et setter

--- src/entity/User.java
+++ src/entity/User.java
@@ -20,4 +20,46 @@ public class User {
         this.password = password;
     }
 
+    // Getter and setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+
 }

0c6e0c9796563060d635be02e69fbcad2ee0c166
Jonathan Maier <jonathan.maier@fms-ea.com>
2026-01-05 11:48:51 +0100

    ajout de la classe course avec ses attributs

---
+++ src/entity/Course.java
@@ -0,0 +1,15 @@
+package entity;
+
+public class Course {
+    private Long id;
+    private String name;
+    private String description;
+    private int duration;
+    private String type;
+    private double price;
+
+    public  Course() {
+    }
+
+
+}

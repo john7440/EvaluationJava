48604f100b393962817f369b55899319f8fca76b
Jonathan Maier <jonathan.maier@fms-ea.com>
2026-01-05 15:18:26 +0100

    ajout de la classe dao client avec l'implémentation idao

---
+++ src/dao/ClientDao.java
@@ -0,0 +1,47 @@
+package dao;
+
+import config.DatabaseConfig;
+import entity.Client;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * Dao  for Client entity
+ */
+public class ClientDao implements IDao<Client> {
+    private static final Logger LOGGER = Logger.getLogger(ClientDao.class.getName());
+    private static ClientDao instance;
+    private DatabaseConfig dbConfig;
+
+    private ClientDao() throws IOException, ClassNotFoundException {
+        this.dbConfig = DatabaseConfig.getInstance();
+    }
+    
+    @Override
+    public Client save(Client entity) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Client update(Client entity) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+
+    @Override
+    public Client findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Client> findAll() throws SQLException {
+        return List.of();
+    }
+}

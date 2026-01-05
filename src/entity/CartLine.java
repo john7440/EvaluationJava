0da0ee38806a0582858b371a8d4e4237710d3a36
Jonathan Maier <jonathan.maier@fms-ea.com>
2026-01-05 12:56:56 +0100

    ajout du constructeur + getter et setter

--- src/entity/CartLine.java
+++ src/entity/CartLine.java
@@ -10,4 +10,42 @@ public class CartLine {
     public CartLine() {
         this.quantity = 1;
     }
+
+    public CartLine(Cart cart, Course course, int quantity) {
+        this.cart = cart;
+        this.course = course;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
 }

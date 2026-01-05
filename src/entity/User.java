13934ec18d84b68d48f1280c7863cc91917ede87
Jonathan Maier <jonathan.maier@fms-ea.com>
2026-01-05 11:30:12 +0100

    ajout des attributs + constructeur

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,4 +1,23 @@
 package entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private Long id;
+    private String login;
+    private String password;
+    private Cart cart;
+    private List<Order> orders;
+
+    public User() {
+        this.orders = new ArrayList<>();
+    }
+
+    public User(String login, String password) {
+        this();
+        this.login = login;
+        this.password = password;
+    }
+
 }

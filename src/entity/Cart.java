7da331a5126aac8592777debb2d79885bcf5244a
Jonathan Maier <jonathan.maier@fms-ea.com>
2026-01-05 11:55:12 +0100

    ajout des initialisation de la date + list et constructeur

--- src/entity/Cart.java
+++ src/entity/Cart.java
@@ -1,10 +1,24 @@
 package entity;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Cart {
     private Long id;
     private Date createdDate;
     private User user;
+    private List<CartLine> cartLines;
+
+    public Cart() {
+        this.createdDate = new Date();
+        this.cartLines = new ArrayList<>();
+    }
+
+    public Cart(User user) {
+        this();
+        this.user = user;
+
+    }
 
 }

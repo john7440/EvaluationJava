6715c79e8b9337364d4e1acda48e671af84aaffb
Jonathan Maier <jonathan.maier@fms-ea.com>
2026-01-05 11:40:10 +0100

    ajout d'une classe client + attributs

---
+++ src/entity/Client.java
@@ -0,0 +1,13 @@
+package entity;
+
+import java.util.List;
+
+public class Client {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String address;
+    private String phoneNumber;
+    private List<Order> orders;
+}

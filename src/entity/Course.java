0f5fd7699d93a8da3ba3fa2055d5c34a1b70694d
Jonathan Maier <jonathan.maier@fms-ea.com>
2026-01-05 11:50:16 +0100

    ajout du constructeur + getter et setter

--- src/entity/Course.java
+++ src/entity/Course.java
@@ -11,5 +11,59 @@ public class Course {
     public  Course() {
     }
 
+    public Course(String name, String description, int duration, String type, double price) {
+        this.name = name;
+        this.description = description;
+        this.duration = duration;
+        this.type = type;
+        this.price = price;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }

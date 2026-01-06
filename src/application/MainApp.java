d8a00a7eae2e22c1583b750db48a13f53bca906a
Jonathan Maier <jonathan.maier@fms-ea.com>
2026-01-06 12:29:26 +0100

    ajout de la premiere partie du menu visiteur + la methode pour voir tous les cours

--- src/application/MainApp.java
+++ src/application/MainApp.java
@@ -7,6 +7,8 @@ import business.UserBusiness;
 import entity.*;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
@@ -55,5 +57,54 @@ public class MainApp {
         }
         System.out.println("\nThank you for using our Application!");
         scanner.close();
+    }
+
+    /**
+     * Display menu for visitors (non-authenticated users)
+     */
+    private boolean displayVisitorMenu() throws SQLException, ClassNotFoundException {
+        System.out.println("\n========== VISITOR MENU ==========");
+        System.out.println("1. View all courses");
+        System.out.println("2. Search courses by keyword");
+        System.out.println("3. Filter courses by type");
+        System.out.println("4. Create an account");
+        System.out.println("5. Login");
+        System.out.println("0. Exit");
+        System.out.println("==================================");
+        System.out.print("Choose an option: ");
+
+        int choice = readInt();
+
+        switch (choice) {
+            case 1:
+                viewAllCourses();
+                break;
+            case 0:
+                return false;
+            default:
+                System.out.println("Invalid option. Please try again.");
+        }
+        return true;
+    }
+
+    /**
+     * View all available courses
+     */
+    private void viewAllCourses() throws SQLException, ClassNotFoundException {
+        List<Course> courses = courseBusiness.getAllCourses();
+        courseBusiness.displayCourseList(courses);
+    }
+
+    /**
+     * Read integer input with error handling
+     */
+    private int readInt() {
+        try {
+            int value = Integer.parseInt(scanner.nextLine());
+            return value;
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a number.");
+            return -1;
+        }
     }
 }

567e0a23984711ab12c00147763d370dbef363a0
Jonathan Maier <jonathan.maier@fms-ea.com>
2026-01-06 08:28:53 +0100

    ajout de la classe dao pour les commandes

---
+++ src/dao/OrderDao.java
@@ -0,0 +1,39 @@
+package dao;
+
+import config.DatabaseConfig;
+import entity.Order;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class OrderDao implements IDao<Order>{
+    private static final Logger LOGGER = Logger.getLogger(OrderDao.class.getName());
+    private static OrderDao instance;
+    private DatabaseConfig dbConfig;
+
+    @Override
+    public Order save(Order entity) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Order update(Order entity) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+
+    @Override
+    public Order findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Order> findAll() throws SQLException {
+        return List.of();
+    }
+}

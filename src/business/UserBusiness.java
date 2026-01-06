83d03c7411bbca975d974a762068c60461f2432a
Jonathan Maier <jonathan.maier@fms-ea.com>
2026-01-06 09:31:27 +0100

    ajout de la méthode pour créer un compte + verfications

--- src/business/UserBusiness.java
+++ src/business/UserBusiness.java
@@ -2,8 +2,11 @@ package business;
 
 import dao.DaoFactory;
 import dao.UserDao;
+import entity.User;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -15,5 +18,29 @@ public class UserBusiness {
 
     public  UserBusiness() throws IOException, ClassNotFoundException {
         this.userDao = DaoFactory.getUserDao();
+    }
+
+    public User createAccount(String login, String password) throws SQLException {
+        //check if login or password are not empty
+        if (login == null || login.trim().isEmpty()) {
+            LOGGER.log(Level.WARNING, "Login cannot be empty!");
+            return null;
+        }
+
+        if (password == null || password.trim().isEmpty()) {
+            LOGGER.log(Level.WARNING, "Password cannot be empty!");
+            return null;
+        }
+
+        //check if this login is already taken
+        User existingUser = userDao.findByLogin(login);
+        if (existingUser != null) {
+            LOGGER.log(Level.WARNING, "Login already exists!");
+            return null;
+        }
+
+        //new user
+        User user = new User(login, password);
+        return userDao.save(user);
     }
 }
